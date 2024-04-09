8cc56b8e201f09199e6d9cfa69c62d06efd2f403
DDoudine <amandinekemp@gmail.com>
2024-04-09 16:35:40 +0200

    Add a script that starts a Flask web application

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Start a Flask web application"""
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+	"""Display a message"""
+	return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+	"""Display a message"""
+	return "HBNB"
+
+
+if __name__ == '__main__':
+	app.run(port=5000, host='0.0.0.0')
