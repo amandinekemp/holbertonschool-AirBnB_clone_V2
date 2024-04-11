5c24a632ad6b2af13dbae881680da82e1c6c469d
DDoudine <amandinekemp@gmail.com>
2024-04-11 17:36:47 +0200

    Fix pycodestyle

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -7,17 +7,17 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
-def hello_hbnb():
-	"""Display a message"""
-	return "Hello HBNB!"
+@app.route("/", strict_slashes=False)
+def hello_HBNB():
+    """Display a message"""
+    return "Hello HBNB!"
 
 
-@app.route('/hbnb', strict_slashes=False)
+@app.route("/hbnb", strict_slashes=False)
 def hbnb():
-	"""Display a message"""
-	return "HBNB"
+    """Display a message"""
+    return "HBNB"
 
 
-if __name__ == '__main__':
-	app.run(port=5000, host='0.0.0.0')
+if __name__ == "__main__":
+    app.run(port=5000, host="0.0.0.0")
--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -6,21 +6,24 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
+@app.route("/", strict_slashes=False)
 def hello_HBNB():
-	"""Display a message"""
-	return "Hello HBNB!"
+    """Display a message"""
+    return "Hello HBNB!"
 
-@app.route('/hbnb', strict_slashes=False)
+
+@app.route("/hbnb", strict_slashes=False)
 def HBNB():
-	"""Display a message"""
-	return "HBNB"
+    """Display a message"""
+    return "HBNB"
 
-@app.route('/c/<text>', strict_slashes=False)
+
+@app.route("/c/<text>", strict_slashes=False)
 def C_text(text):
-	"""Display a message"""
-	text = text.replace('_', ' ')
-	return f"C {text}"
+    """Display a message"""
+    text = text.replace("_", " ")
+    return f"C {text}"
 
-if __name__ == '__main__':
-	app.run(port=5000, host='0.0.0.0')
+
+if __name__ == "__main__":
+    app.run(port=5000, host="0.0.0.0")
