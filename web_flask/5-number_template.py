e14bcb7637b73183fcf1f9f572fe37b5b1ab15e2
DDoudine <amandinekemp@gmail.com>
2024-04-13 09:49:06 +0200

    Move directory template in web_flask

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -39,10 +39,10 @@ def number(n):
     return "{} is a number".format(n)
 
 
-@app.route("/number_template/<int:n>", strict_slashes=False)
-def number_template(n):
+@app.route("/number_template/<int:num>", strict_slashes=False)
+def number_template(num):
     """Display a message"""
-    return render_template("5-number.html", n=n)
+    return render_template('5-number.html', n=num)
 
 
 if __name__ == "__main__":
